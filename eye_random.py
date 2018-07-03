d694635f39834650da8463781194bcbd5cdc8867
Ryan Stong <rvanstong@gmail.com>
2018-07-03 08:45:55 -0700

    adds in eye_random for rand eye movement animation, changed eye.py to eye_acc to reflect use of accelerometer

---
+++ eye_random.py
@@ -0,0 +1,25 @@
+from microbit import *
+import random
+
+eyelid = Image("05550:"
+                "50005:"
+                "00000:"
+                "50005:"
+                "05550")
+sensitivity = 500
+wait_period = 1500
+
+def draw_pupil(prev_x, prev_y, x,y):
+    display.set_pixel(x,y,9)
+    sleep(wait_period)
+
+while True:
+    x = random.randint(1,3)
+    y = random.randint(1,3)
+
+
+    display.show(eyelid)
+    draw_pupil(0,0,x,y)
+    prev_x = x
+    prev_y = y
+    draw_pupil(prev_x,prev_y,x,y)
