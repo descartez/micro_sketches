4448c54af1ff2248f12f55545553638060e75372
Ryan Stong <rvanstong@gmail.com>
2018-08-22 07:01:00 -0700

    updates badge to display URL for odo project

--- microbit/badge.py
+++ microbit/badge.py
@@ -2,13 +2,11 @@ from microbit import *
 
 flash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
 display.show(flash, delay=100)
-accelerometerRunning= False
 
 while True:
-    if accelerometer.was_gesture("shake") and accelerometerRunning:
-        display.show(flash, delay=100, wait=False)
-    if button_b.is_pressed() and accelerometerRunning == False:
-        accelerometerRunning = True
-    elif button_b.is_pressed() and accelerometerRunning == True:
-        accelerometerRunning = False
+    display.scroll("PickOdo.com")
+    display.show(flash, delay=100)
+    sleep(300)
+    display.show(flash, delay=100)
+
 
